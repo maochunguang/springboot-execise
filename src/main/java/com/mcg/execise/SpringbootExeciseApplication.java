6db24ca82d597b5010803602e6abb4bfcd150ba9
maochunguang <mcg915881127@163.com>
2021-01-03 21:58:48 +0800

    增加异步线程池

--- src/main/java/com/mcg/execise/SpringbootExeciseApplication.java
+++ src/main/java/com/mcg/execise/SpringbootExeciseApplication.java
@@ -2,6 +2,7 @@ package com.mcg.execise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 public class SpringbootExeciseApplication {
---
+++ src/main/java/com/mcg/execise/extension/VisibleThreadPoolTaskExecutor.java
@@ -0,0 +1,67 @@
+package com.mcg.execise.extension;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author mao
+ * Date：2021/1/3
+ * Description：
+ */
+@Slf4j
+public class VisibleThreadPoolTaskExecutor extends ThreadPoolTaskExecutor {
+    @Override
+    public void execute(Runnable task) {
+        showThreadPoolInfo("[1] do executor");
+        super.execute(task);
+    }
+
+    @Override
+    public void execute(Runnable task, long startTimeout) {
+        showThreadPoolInfo("[2] do executor");
+        super.execute(task, startTimeout);
+    }
+
+    @Override
+    public Future<?> submit(Runnable task) {
+        showThreadPoolInfo("[1] do submit");
+        return super.submit(task);
+    }
+
+    @Override
+    public <T> Future<T> submit(Callable<T> task) {
+        showThreadPoolInfo("[2] do submit");
+        return super.submit(task);
+    }
+
+    @Override
+    public ListenableFuture<?> submitListenable(Runnable task) {
+        showThreadPoolInfo("[1] do submitListenable");
+
+        return super.submitListenable(task);
+    }
+
+    @Override
+    public <T> ListenableFuture<T> submitListenable(Callable<T> task) {
+        showThreadPoolInfo("[2] do submitListenable");
+        return super.submitListenable(task);
+    }
+
+    private void showThreadPoolInfo(String prefix){
+        ThreadPoolExecutor threadPoolExecutor = getThreadPoolExecutor();
+        log.info("name:{},{},taskCount:[{}], completedTask Count[{}], activeCount:[{}],queueSize:[{}]",
+                this.getThreadNamePrefix(),
+                prefix,
+                threadPoolExecutor.getTaskCount(),
+                threadPoolExecutor.getCompletedTaskCount(),
+                threadPoolExecutor.getActiveCount(),
+                threadPoolExecutor.getQueue().size());
+
+    }
+
+}
