132467f90ce36c97ff7c8e6ff621257977bd5af4
maochunguang <mcg915881127@163.com>
2021-01-11 23:56:54 +0800

    增加mybatis分页插件

---
+++ src/main/java/com/mcg/execise/extension/mybatis/MybatisPagePlugin.java
@@ -0,0 +1,68 @@
+package com.mcg.execise.extension.mybatis;
+
+import org.apache.ibatis.builder.StaticSqlSource;
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.SqlSource;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.DefaultReflectorFactory;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
+import org.apache.ibatis.reflection.wrapper.DefaultObjectWrapperFactory;
+import org.apache.ibatis.session.RowBounds;
+
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.util.Properties;
+
+/**
+ * @author mao
+ * Date：2021/1/11
+ * Description：mybatis分页插件练习
+ */
+@Intercepts(@Signature(type = StatementHandler.class, method = "prepare",
+        args = {Connection.class, Integer.class}))
+public class MybatisPagePlugin implements Interceptor {
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        System.out.println("将逻辑分页改为物理分页");
+        Object[] args = invocation.getArgs();
+        MappedStatement ms = (MappedStatement) args[0]; // MappedStatement
+        BoundSql boundSql = ms.getBoundSql(args[1]); // Object parameter
+        RowBounds rb = (RowBounds) args[2]; // RowBounds
+        // RowBounds为空，无需分页
+        if (rb == RowBounds.DEFAULT) {
+            return invocation.proceed();
+        }
+
+        // 将原 RowBounds 参数设为 RowBounds.DEFAULT，关闭 MyBatis 内置的分页机制
+        //args[2] = RowBounds.DEFAULT;
+
+        // 在SQL后加上limit语句
+        String sql = boundSql.getSql();
+        String limit = String.format("LIMIT %d,%d", rb.getOffset(), rb.getLimit());
+        sql = sql + " " + limit;
+
+        // 自定义sqlSource
+        SqlSource sqlSource = new StaticSqlSource(ms.getConfiguration(), sql, boundSql.getParameterMappings());
+
+        // 修改原来的sqlSource
+        Field field = MappedStatement.class.getDeclaredField("sqlSource");
+        field.setAccessible(true);
+        field.set(ms, sqlSource);
+
+        return invocation.proceed();
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        return Plugin.wrap(target, this);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+}
